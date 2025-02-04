ee9fdaa9f4d7e1e6462b27d51ed9b79909d6da9c
David Jacot <david.jacot@gmail.com>
2025-02-04 10:46:58 +0100

    wip

--- tests/kafkatest/utils/util.py
+++ tests/kafkatest/utils/util.py
@@ -58,6 +58,7 @@ def is_version(node, version_list, proc_grep_string="kafka", logger=None):
     A useful tool to aid in checking that service version apis are working correctly.
     """
     lines = [l for l in node.account.ssh_capture("ps ax | grep %s | grep -v grep" % proc_grep_string)]
+    print(lines)
     assert len(lines) == 1
     psLine = lines[0]
 
